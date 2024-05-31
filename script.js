336d4c9e7ff9c118258345288a96af02e84ceb85
Turpal <salangeriev97@mail.ru>
2024-05-31 10:15:45 +0300

    homeWork_1

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 'use strict';
 
+let numberOfFilms = prompt('Сколько фильмов  вы уже посмотрели?', '');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+}
+
+let answerOne = prompt('Один из последних просмотренных фильмов?', '');
+let answerTwo = prompt('На сколько оцените его?', '');
+personalMovieDB.movies[answerOne] = `${answerTwo}`;
+console.log(personalMovieDB);
