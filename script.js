7d86bb3ab3f54b50897fe53c0aca58924a0877bb
Turpal <salangeriev97@mail.ru>
2024-06-05 16:07:25 +0300

    homeWork_2

--- script.js
+++ script.js
@@ -9,7 +9,24 @@ const personalMovieDB = {
     privat: false
 }
 
-let answerOne = prompt('Один из последних просмотренных фильмов?', '');
-let answerTwo = prompt('На сколько оцените его?', '');
-personalMovieDB.movies[answerOne] = `${answerTwo}`;
+
+
 console.log(personalMovieDB);
+for (let i = 0; i < 2; i++) {
+    let answerOne = prompt('Один из последних просмотренных фильмов?', '');
+    let answerTwo = prompt('На сколько оцените его?', '');
+
+    if (answerOne == '' && answerTwo == '' && answerOne === null && answerTwo === null & answerOne.length > 50) {
+        i--;
+
+    } else {
+        personalMovieDB.movies[answerOne] = `${answerTwo}`;
+    }
+}
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы классическтй зритель');
+} else {
+    console.log('Error');
+}
